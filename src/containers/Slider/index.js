c59bb8064b1cd860328f432877cb7e35c5dea6e1
gregory-lopez <gregory.lopez@laplateforme.io>
2024-06-23 15:51:30 +0200

    Site debug

---
+++ src/containers/Slider/index.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import { useData } from "../../contexts/DataContext";
+import { getMonth } from "../../helpers/Date";
+
+import "./style.scss";
+
+const Slider = () => {
+  const { data } = useData();
+  const [index, setIndex] = useState(0);
+  const byDateDesc = data?.focus.sort((evtA, evtB) =>
+    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+  );
+
+  const nextCard = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % byDateDesc.length);
+  };
+
+  useEffect(() => {
+    const timer = setInterval(nextCard, 5000);
+    return () => clearInterval(timer);
+  });
+
+  return (
+    <div className="SlideCardList">
+      {byDateDesc?.map((event, idx) => (
+        <div key={event.id}>
+          <div
+            className={`SlideCard SlideCard--${
+              index === idx ? "display" : "hide"
+            }`}
+          >
+            <img src={event.cover} alt="forum" />
+            <div className="SlideCard__descriptionContainer">
+              <div className="SlideCard__description">
+                <h3>{event.title}</h3>
+                <p>{event.description}</p>
+                <div>{getMonth(new Date(event.date))}</div>
+              </div>
+            </div>
+          </div>
+          <div className="SlideCard__paginationContainer">
+            <div className="SlideCard__pagination">
+              {byDateDesc.map((Pagination, radioIdx) => (
+                <input
+                  key={`${event.id}-${Pagination.id}`}
+                  type="radio"
+                  name={`radio-button-${event.id}`}
+                  checked={index === radioIdx}
+                  onChange={() => setIndex(radioIdx)}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Slider;
